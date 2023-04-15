5ef898ad777328aa773a8da1d5913bca4a8a9b5a
Adrian Langmo Pavlak <adrianlp@stud.ntnu.no>
2023-04-15 17:18:54 +0200

    Adding files to repo.

---
+++ Helikopter_code/Dag 1/init_heli_1_2.m
@@ -0,0 +1,36 @@
+% FOR HELICOPTER NR 1-2
+% This file contains the initialization for the helicopter assignment in
+% the course TTK4115. Run this file before you execute QuaRC_ -> Build 
+% to build the file heli_q8.mdl.
+
+% Oppdatert høsten 2006 av Jostein Bakkeheim
+% Oppdatert høsten 2008 av Arnfinn Aas Eielsen
+% Oppdatert høsten 2009 av Jonathan Ronen
+% Updated fall 2010, Dominik Breu
+% Updated fall 2013, Mark Haring
+% Updated spring 2015, Mark Haring
+
+
+%%%%%%%%%%% Calibration of the encoder and the hardware for the specific
+%%%%%%%%%%% helicopter
+Joystick_gain_x = 1;
+Joystick_gain_y = -1;
+
+
+%%%%%%%%%%% Physical constants
+g = 9.81; % gravitational constant [m/s^2]
+l_c = 0.40; % distance elevation axis to counterweight [m]
+l_h = 0.66; % distance elevation axis to helicopter head [m]
+l_p = 0.175; % distance pitch axis to motor [m]
+m_c = 1.92; % Counterweight mass [kg]
+m_p = 0.65; % Motor mass [kg]
+
+
+%%%%%%%%%%% Magical constants
+offset_elevation = 0.428;
+v_s0 = 6.53685;
+k_f = -(g*(l_c*m_c-2*l_h*m_p))/(l_h*v_s0);
+k_pp = 35.419;
+k_pd = 30;
+k_pd_squared = k_pd*k_pd;
+k_pp_part = 8*k_pp*m_p/k_f;
